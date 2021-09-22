f918a27a75b310c26d87f79781883c1349db68d1
Mullosharaf <52775855+CodeHunterOfficial@users.noreply.github.com>
2021-09-22 11:46:05 +0300

    Add files via upload

---
+++ SQLQueryProdects.sql
@@ -0,0 +1,49 @@
+CREATE TABLE Products (
+	Id INT  IDENTITY(1,1) NOT NULL,
+	Name nvarchar(50),
+	Primary Key(Id)
+);
+
+INSERT INTO Products
+VALUES
+	('Java'),
+	('C#'),
+	('Opel'),
+	('KIA '),
+	('Range Rower'),
+	('Thosihiba');
+
+CREATE TABLE Categories (
+	Id INT  IDENTITY(1,1) NOT NULL,
+	Name nvarchar(50),
+	Primary Key(Id)
+);
+
+INSERT INTO Categories
+VALUES
+	('Book'),
+	('Car'),
+	('Computer');
+
+CREATE TABLE ProductCategories (
+    Id INT  IDENTITY(1,1) NOT NULL,
+	ProductId INT FOREIGN KEY REFERENCES Products(Id),
+	CategoryId INT FOREIGN KEY REFERENCES Categories(Id),
+	Primary Key(Id)
+);
+
+INSERT INTO ProductCategories
+VALUES
+	(1, 1),
+	(2, 1),
+	(3, 2),
+	(4,2),
+	(5,2),
+	(6,3);
+
+SELECT P.Name, C.Name
+FROM Products P
+LEFT JOIN ProductCategories PC
+	ON P.Id = PC.ProductId
+LEFT JOIN Categories C
+	ON PC.CategoryId = C.Id;
